d580d3d7c942c748a1dc2730971a36448a94f859
WenHao2130 <wenhao2130@outlook.com>
2026-06-24 00:52:50 +0800

    OP46B1: init: Add motor_calib one-shot
    Add a root one-shot that reads the per-device factory motor hall
    calibration from the opporeserve1 reserve partition (24 bytes = 6x
    int32 LE @ 0x434000) and writes it to /sys/class/motor/hall_calibration
    on boot, reproducing what the official engineer HAL does. Skip all-zero
    (invalid) records. Running as root lets it read the 0600 block node
    directly, where the system-uid camera_helper failed with EACCES.
    
    Change-Id: Ifaa6075907fbf9a4106262a6b3681da46bb2b596
    Signed-off-by: WenHao2130 <wenhao2130@outlook.com>

--- OP46B1-vendor.mk
+++ OP46B1-vendor.mk
@@ -241,7 +241,6 @@ PRODUCT_COPY_FILES += \
     vendor/oppo/OP46B1/proprietary/vendor/etc/init/dataadpl.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/dataadpl.rc \
     vendor/oppo/OP46B1/proprietary/vendor/etc/init/dataqti.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/dataqti.rc \
     vendor/oppo/OP46B1/proprietary/vendor/etc/init/dpmQmiMgr.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/dpmQmiMgr.rc \
-    vendor/oppo/OP46B1/proprietary/vendor/etc/init/hw/init.oppo.vendor.motor.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.oppo.vendor.motor.rc \
     vendor/oppo/OP46B1/proprietary/vendor/etc/init/ims_rtp_daemon.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/ims_rtp_daemon.rc \
     vendor/oppo/OP46B1/proprietary/vendor/etc/init/imsdatadaemon.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/imsdatadaemon.rc \
     vendor/oppo/OP46B1/proprietary/vendor/etc/init/imsqmidaemon.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/imsqmidaemon.rc \
